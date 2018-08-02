dac49c7245b23a00dbc4481af919b22e6fd67930
junxian shen <sjx15@mails.tsinghua.edu.cn>
2018-08-02 15:58:01 +0800

    stable version

--- gui/register/renderer.js
+++ gui/register/renderer.js
@@ -11,30 +11,31 @@ client.invoke("echo", "server ready", (error, res) => {
   }
 })
 
-/*
+
 let name = document.querySelector('#name')
 var sex = document.getElementsByName("sex");
-
-// let ty = document.querySelector('#ty')
+var ty = document.getElementsByName("ty");
 let department = document.querySelector('#dep')
 let ID = document.querySelector('#id')
 let start_date = document.querySelector('#start_date')
 let end_date = document.querySelector('#end_date')
-*/
+
 let submit_button = document.querySelector('#create_button')
 
 submit_button.addEventListener('click', () => {
-  /*
-  var sex_val = "0";
+  var sex_val;
   for (i = 0; i < sex.length; i ++) {
     if (sex[i].checked) {
       sex_val = i.toString();
     }
   }
-  alert(sex.length);
-  */
-  client.invoke("create_card", "1", "?", "3", "2", "3", "4", "5", (error, res) => {
-//  client.invoke("create_card", name.value, "?", "3", department.value, ID.value, start_date.value, end_date.value, (error, res) => {
+  var ty_val;
+  for (i = 0; i < ty.length; i ++) {
+    if (ty[i].checked) {
+      ty_val = i.toString();
+    }
+  }
+  client.invoke("create_card", name.value, sex_val, ty_val, department.value, ID.value, start_date.value, end_date.value, (error, res) => {
       if(error) {
 //      console.error(error)
     } else {
