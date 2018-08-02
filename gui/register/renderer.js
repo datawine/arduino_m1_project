3bc9242b7cbc05d0feb9ddd69919dbadd1565611
wasabbi <lee.yeb33@gmail.com>
2018-08-02 05:51:20 -0700

    register gui upgrade
    Signed-off-by: wasabbi <lee.yeb33@gmail.com>

--- gui/register/renderer.js
+++ gui/register/renderer.js
@@ -11,7 +11,7 @@ client.invoke("echo", "server ready", (error, res) => {
   }
 })
 
-
+/*注册新卡*/
 let name = document.querySelector('#name')
 var sex = document.getElementsByName("sex");
 var ty = document.getElementsByName("ty");
@@ -22,39 +22,53 @@ let end_date = document.querySelector('#end_date')
 
 let submit_button = document.querySelector('#create_button')
 
+/*延长有效期*/
 let end_date2 = document.querySelector('#end_date_2');
 let extend_button = document.querySelector('#extend_button')
 
+/*获得信息*/
+let cardinfo = document.querySelector('#cardinfo');		//输入框
+let cardinfo_button = document.querySelector('#cardinfo_button')
+
+/*注销旧卡*/
 var cb1 = document.getElementsByName("checkbox1");
 var cb2 = document.getElementsByName("checkbox2");
 let clear_button = document.querySelector('#clear_button')
+
+/*添加门禁*/
+let doorid = document.querySelector('#doorid');			//输入框
+let door_button = document.querySelector('#door_button')
+
+/*添加门禁*/
+let doorid2 = document.querySelector('#doorid2');		//输入框
+let door_button2 = document.querySelector('#door_button2')
+
+
 
 submit_button.addEventListener('click', () => {
   var sex_val;
   for (i = 0; i < sex.length; i ++) {
     if (sex[i].checked) {
-      sex_val = (i+1).toString();
+      sex_val = i.toString();
     }
   }
   var ty_val;
   for (i = 0; i < ty.length; i ++) {
     if (ty[i].checked) {
-      ty_val = (i+1).toString();
+      ty_val = i.toString();
     }
   }
-  client.invoke("create_new", name.value, sex_val, ty_val, department.value, ID.value, start_date.value, end_date.value, (error, res) => {
+  client.invoke("create_card", name.value, sex_val, ty_val, department.value, ID.value, start_date.value, end_date.value, (error, res) => {
       if(error) {
     } else {
-      alert(res);
     }
   })
 })
 
 extend_button.addEventListener('click', () => {
-  client.invoke("refresh_card", end_date2.value, (error, res) => {
+  client.invoke("create_card", end_date2.value, (error, res) => {
       if(error) {
     } else {
-      alert(res);
     }
   })
 })
@@ -62,17 +76,12 @@ extend_button.addEventListener('click', () => {
 clear_button.addEventListener('click', () => {
   var cb1_val, cb2_val;
   if (cb1.checked)
-    cb1_val = "1";
-  else
-    cb1_val = "2";
+    cb1_val = "?";
   if (cb2.checked)
-    cb2_val = "1";
-  else
-    cb2_val = "2";
-  client.invoke("clear_cards", cb1_val, cb2_val, (error, res) => {
+    cb2_val = "?";
+  client.invoke("create_card", cb1_val, cb2_val, (error, res) => {
       if(error) {
     } else {
-      alert(res);
     }
   })
 })
