8b0f07481bcfdc3995c8965887589a3222c18588
wasabbi <lee.yeb33@gmail.com>
2018-08-02 07:54:27 -0700

    money gui: update output tag
    Signed-off-by: wasabbi <lee.yeb33@gmail.com>

--- gui/money/renderer.js
+++ gui/money/renderer.js
@@ -11,29 +11,35 @@ client.invoke("echo", "server ready", (error, res) => {
   }
 })
 
-/*查询余额*/
-//没有输入, 只有输出:
-let balance = document.querySelector('#balance')   //余额
+/*综合信息*/
+//无输入, 只有输出:
+let name = document.querySelector('#name')
+let dep = document.querySelector('#dep')			//院系
+let ID = document.querySelector('#ID')
+let ty = document.querySelector('#ty')				//类别
+let start_date = document.querySelector('#start_date')
+let end_date = document.querySelector('#end_date')
+let balance = document.querySelector('#balance')		//余额
 
 
 /*圈存*/
 //输入:
-let seller1 = document.querySelector('#seller1')
-let recharge = document.querySelector('#recharge')
+let recharge = document.querySelector('#recharge')		//充值金额
 let recharge_button = document.querySelector('#recharge_button')
 //输出:
-//let balance = document.querySelector('#balance')   //余额, 和"查看余额"重复不用再声明
+//let balance = document.querySelector('#balance')		//与"综合信息"重合
 
-/*消费*/
+
+/*挂失获取金额*/
 //输入:
-let seller2 = document.querySelector('#seller2')
-let consume = document.querySelector('#consume')
-let consume_button = document.querySelector('#consume_button')
+let return_button = document.querySelector('#return_button')
 //输出:
-//let balance = document.querySelector('#balance')   //余额
+let lose_date = document.querySelector('#lose_date')		//挂失日期
+let lose_money = document.querySelector('#lose_money')		//挂失金额
+
 
 /*消费记录*/
-//没有输入, 只有输出:
+//无输入, 只有输出:
 let date1 = document.querySelector('#date1')   //第一个消费日期
 let price1 = document.querySelector('#price1')   //第一个消费金额
 let date2 = document.querySelector('#date2')   //第二个消费日期
@@ -44,6 +50,8 @@ let date4 = document.querySelector('#date4')
 let price4 = document.querySelector('#price4')
 let date5 = document.querySelector('#date5')
 let price5 = document.querySelector('#price5')
+
+
 
 /*
 submit_button.addEventListener('click', () => {
